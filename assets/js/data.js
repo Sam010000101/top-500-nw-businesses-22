d1c2198e1920bc765f272cef203d44af593e2a7f
Sam B <reposme@outlook.com>
2023-01-21 13:33:49 +0000

    Inculsion of year end data to const companyData

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,61 +3,71 @@ const companyData = [
         "company": "JD Sports",
         "location": "Bury",
         "turnover": 8563.0,
-        "pre-tax proffit": 654.7
+        "pre-tax proffit": 654.7,
+        "year end": "Jan 22"
     },
     {
         "company": "B&M Retail",
         "location": "Liverpool",
         "turnover": 3914.0,
-        "pre-tax proffit": 514.0
+        "pre-tax proffit": 514.0,
+        "year end": "Mar 22"
     },
     {
         "company": "EG",
         "location": "Blackburn",
         "turnover": 19553.4,
-        "pre-tax proffit": 153.2
+        "pre-tax proffit": 153.2,
+        "year end": "Dec 21"
     },
     {
         "company": "Inovyn",
         "location": "Runcorn",
         "turnover": 3547.4,
-        "pre-tax proffit": 466.8
+        "pre-tax proffit": 466.8,
+        "year end": "Dec 21"
     },
     {
         "company": "Ausurus",
         "location": "Warrington",
         "turnover": 4695.0,
-        "pre-tax proffit": 251.0
+        "pre-tax proffit": 251.0,
+        "year end": "Dec 21"
     },
     {
         "company": "Bentley Motors",
         "location": "Crewe",
         "turnover": 2392.8,
-        "pre-tax proffit": 560.7
+        "pre-tax proffit": 560.7,
+        "year end": "Dec 21"
     },
     {
         "company": "TJ Morris",
         "location": "Liverpool",
         "turnover": 3338.0,
-        "pre-tax proffit": 396.7
+        "pre-tax proffit": 396.7,
+        "year end": "Jun 21"
     },
     {
         "company": "Co-Operative",
         "location": "Manchester",
         "turnover": 11427.0,
-        "pre-tax proffit": 127.0
+        "pre-tax proffit": 127.0,
+        "year end": "Jan 21"
     },
     {
         "company": "Marlowe",
         "location": "Knutsford",
         "turnover": 2737.8,
-        "pre-tax proffit": 167.1
+        "pre-tax proffit": 167.1,
+        "year end": "Dec 21"
     },
     {
         "company": "United Utilities",
         "location": "Warrington",
         "turnover": 1862.7,
-        "pre-tax proffit": 439.9
+        "pre-tax proffit": 439.9,
+        "year end": "Mar 22"
     }
 
 ];
